0bee2ecefbfffa06d7a68b7c3028765ccd9ded4d
Smoke <86024507+crypto-smoke@users.noreply.github.com>
2024-02-18 06:18:32 -1000

    cleanup lora config, setup radio for rx by default

--- hardware/waveshare-2040-lora.go
+++ hardware/waveshare-2040-lora.go
@@ -16,11 +16,7 @@ func init() {
 func ConfigureLoRa() (*sx126x.Device, error) {
 
 	err := machine.SPI1.Configure(machine.SPIConfig{
-		//Mode:      0,
 		Frequency: 8 * 1e6,
-		//SDO: machine.GPIO15,
-		//SDI: machine.GPIO24,
-		//SCK: machine.GPIO14,
 	})
 	if err != nil {
 		return nil, err
@@ -29,22 +25,19 @@ func ConfigureLoRa() (*sx126x.Device, error) {
 	machine.LORA_RESET.Configure(machine.PinConfig{Mode: machine.PinOutput})
 	machine.LORA_ANT_SW.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
-	// trigger a radio reset
-	//machine.LORA_RESET.Low()
-	//time.Sleep(100 * time.Nanosecond)
-	machine.LORA_RESET.High()
+	machine.LORA_RESET.High() // not in reset
 
 	loraRadio := sx126x.New(machine.SPI1)
 	loraRadio.SetDeviceType(sx126x.DEVICE_TYPE_SX1262)
 	controller := sx126x.NewRadioControl(
 		machine.LORA_CS, machine.LORA_BUSY, machine.LORA_DIO1,
 		machine.LORA_ANT_SW, machine.NoPin, machine.NoPin)
-	//controller.SetRfSwitchMode(sx126x.RFSWITCH_RX)
+	controller.SetRfSwitchMode(sx126x.RFSWITCH_RX)
 	err = loraRadio.SetRadioController(controller)
 	if err != nil {
 		return nil, err
 	}
-	//	loraRadio.ExecSetCommand(sx126x.SX126X_CMD_SET_DIO2_AS_RF_SWITCH_CTRL, []uint8{sx126x.SX126X_DIO2_AS_RF_SWITCH})
+	loraRadio.ExecSetCommand(sx126x.SX126X_CMD_SET_DIO2_AS_RF_SWITCH_CTRL, []uint8{sx126x.SX126X_DIO2_AS_RF_SWITCH})
 
 	return loraRadio, nil
 }
